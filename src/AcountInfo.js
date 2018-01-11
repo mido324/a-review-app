eb46d62e32903e8e633386f21644f1a55f8a7855
mido <mido.ghanim@gmail.com>
2018-01-11 11:00:46 -0700

    adding component

---
+++ src/AcountInfo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AcountInfo extends Component {
+    render() {
+        return (
+            <div>
+                <h1>AcountInfo</h1>
+            </div>
+        );
+    }
+}
+
+export default AcountInfo;
--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { Route} from 'react-router-dom'
 import './App.css';
-
+import Home from './Home';
+import AccountInfo from './AcountInfo';
 class App extends Component {
   render() {
     return (
       <div>
-       <Route path="/" exact component = {home} />
-       <Route path="/private"  component ={accountInfo} />
+       <Route path="/" exact component = {Home} />
+       <Route path="/private"  component ={AccountInfo} />
       </div>
     );
   }
