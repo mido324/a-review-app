d7de3d7c6156fd9c2fbba4f41718638c10de30c8
mido <mido.ghanim@gmail.com>
2018-01-11 10:47:46 -0700

    add routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-
+import { Route} from 'react-router-dom'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       <h1>Hello world</h1>
+      <div>
+       <Route path="/" exact component = {home} />
+       <Route path="/private"  component ={accountInfo} />
       </div>
     );
   }
